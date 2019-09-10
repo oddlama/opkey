99471cfa67a7a2f61a214f586bdf0168a2ad4f7f
oddlama <oddlama@oddlama.org>
2019-09-10 12:47:18 +0200

    bandpass audio

--- plot.py
+++ plot.py
@@ -25,6 +25,51 @@ plotAxisVel = "y2"
 plotAxisVelRaw = "y4"
 plotAxisWav = "y5"
 
+maxKeyFreqDeviation = 0.05
+keyFrequencies = {
+    'A0'    : 27.50000, 'A#0'   : 29.13524, 'B0'    : 30.86771, 'C1'    : 32.70320,
+    'C#1'   : 34.64783, 'D1'    : 36.70810, 'D#1'   : 38.89087, 'E1'    : 41.20344,
+    'F1'    : 43.65353, 'F#1'   : 46.24930, 'G1'    : 48.99943, 'G#1'   : 51.91309,
+    'A1'    : 55.00000, 'A#1'   : 58.27047, 'B1'    : 61.73541, 'C2'    : 65.40639,
+    'C#2'   : 69.29566, 'D2'    : 73.41619, 'D#2'   : 77.78175, 'E2'    : 82.40689,
+    'F2'    : 87.30706, 'F#2'   : 92.49861, 'G2'    : 97.99886, 'G#2'   : 103.8262,
+    'A2'    : 110.0000, 'A#2'   : 116.5409, 'B2'    : 123.4708, 'C3'    : 130.8128,
+    'C#3'   : 138.5913, 'D3'    : 146.8324, 'D#3'   : 155.5635, 'E3'    : 164.8138,
+    'F3'    : 174.6141, 'F#3'   : 184.9972, 'G3'    : 195.9977, 'G#3'   : 207.6523,
+    'A3'    : 220.0000, 'A#3'   : 233.0819, 'B3'    : 246.9417, 'C4'    : 261.6256,
+    'C#4'   : 277.1826, 'D4'    : 293.6648, 'D#4'   : 311.1270, 'E4'    : 329.6276,
+    'F4'    : 349.2282, 'F#4'   : 369.9944, 'G4'    : 391.9954, 'G#4'   : 415.3047,
+    'A4'    : 440.0000, 'A#4'   : 466.1638, 'B4'    : 493.8833, 'C5'    : 523.2511,
+    'C#5'   : 554.3653, 'D5'    : 587.3295, 'D#5'   : 622.2540, 'E5'    : 659.2551,
+    'F5'    : 698.4565, 'F#5'   : 739.9888, 'G5'    : 783.9909, 'G#5'   : 830.6094,
+    'A5'    : 880.0000, 'A#5'   : 932.3275, 'B5'    : 987.7666, 'C6'    : 1046.502,
+    'C#6'   : 1108.731, 'D6'    : 1174.659, 'D#6'   : 1244.508, 'E6'    : 1318.510,
+    'F6'    : 1396.913, 'F#6'   : 1479.978, 'G6'    : 1567.982, 'G#6'   : 1661.219,
+    'A6'    : 1760.000, 'A#6'   : 1864.655, 'B6'    : 1975.533, 'C7'    : 2093.005,
+    'C#7'   : 2217.461, 'D7'    : 2349.318, 'D#7'   : 2489.016, 'E7'    : 2637.020,
+    'F7'    : 2793.826, 'F#7'   : 2959.955, 'G7'    : 3135.963, 'G#7'   : 3322.438,
+    'A7'    : 3520.000, 'A#7'   : 3729.310, 'B7'    : 3951.066, 'C8'    : 4186.009,
+    'Soft'  : 0.0,
+    'Damp'  : 0.0,
+}
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = signal.butter(order, [low, high], btype='band')
+    return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = signal.lfilter(b, a, data)
+    return y
+
+def running_mean(x, N):
+    cumsum = np.cumsum(np.insert(x, 0, 0))
+    return (cumsum[N:] - cumsum[:-N]) / float(N)
+
+
 #def butter_lowpass(cutoff, fs, order):
 #    nyq = 0.5 * fs
 #    normal_cutoff = cutoff / nyq
@@ -59,7 +104,7 @@ def setFigureLayout(fig):
     fig.update_layout(
         xaxis=dict(
             title="Time (ms)",
-            domain=[.04,.96] if includeRaw else [0.0, 1.0],
+            domain=[.04,.92] if includeRaw else [0.0, 1.0],
             ),
         yaxis=dict(
             title="Position",
@@ -95,7 +140,7 @@ def setFigureLayout(fig):
                 overlaying="y",
                 anchor="free",
                 side="right",
-                position=1.0,
+                position=0.96,
                 ),
             )
     fig.update_layout(
@@ -227,27 +272,32 @@ class Capture:
         if self.audioData is None:
             return
 
-        range = (0.9, 1.0 + (self.meta.getElapsedNsMcu() / 1000000000))
-        offset = 1.0
-        first = int(range[0] * self.audioFs)
-        last = int(range[1] * self.audioFs)
-        tWav = np.linspace((range[0] - offset) * 1000, (range[1] - offset) * 1000, num=(last - first))
+        recordOffset = 1.0 - 0.06
 
-        def running_mean(x, N):
-            cumsum = np.cumsum(np.insert(x, 0, 0))
-            return (cumsum[N:] - cumsum[:-N]) / float(N)
+        t0 = self.t0 / 1000000
+        mcuSampleLength = (self.meta.getElapsedNsMcu() / 1000000000)
+
+        readRange = (recordOffset, recordOffset + mcuSampleLength)
+        displayRange = (t0, t0 + mcuSampleLength)
+
+        first = int(readRange[0] * self.audioFs)
+        last = int(readRange[1] * self.audioFs)
+        tWav = np.linspace(displayRange[0] * 1000, displayRange[1] * 1000, num=(last - first))
+
+        lowcut = keyFrequencies[self.sensorName] * (1.0 - maxKeyFreqDeviation)
+        highcut = keyFrequencies[self.sensorName] * (1.0 + maxKeyFreqDeviation)
 
         fig.add_trace(go.Scattergl(
             name='{} wav (l)'.format(self.getIdentifier()),
             x=tWav,
-            y=running_mean(np.absolute(self.audioData[first:last,0]), 200),
+            y=butter_bandpass_filter(self.audioData[first:last,0], lowcut, highcut, self.audioFs),
             yaxis=plotAxisWav,
         ))
 
         fig.add_trace(go.Scattergl(
             name='{} wav (r)'.format(self.getIdentifier()),
             x=tWav,
-            y=running_mean(np.absolute(self.audioData[first:last,1]), 200),
+            y=butter_bandpass_filter(self.audioData[first:last,1], lowcut, highcut, self.audioFs),
             yaxis=plotAxisWav,
         ))
 
